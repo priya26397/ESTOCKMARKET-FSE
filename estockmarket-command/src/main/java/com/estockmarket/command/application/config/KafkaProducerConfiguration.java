e071e71d78dde30b34aacd7fbd803d21c2e4b786
unknown <cogjava182@iiht.tech>
2022-05-15 17:58:42 +0530

    Added test case

--- estockmarket-command/src/main/java/com/estockmarket/command/application/config/KafkaProducerConfiguration.java
+++ estockmarket-command/src/main/java/com/estockmarket/command/application/config/KafkaProducerConfiguration.java
@@ -20,7 +20,7 @@ public class KafkaProducerConfiguration {
 	    public ProducerFactory<String, String> producerFactory() {
 	        Map<String, Object> config = new HashMap<>();
 
-	        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:29092");
+	        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
 	        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 	        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 
