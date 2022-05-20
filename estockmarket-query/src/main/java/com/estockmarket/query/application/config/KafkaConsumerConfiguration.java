daa83c23c85f443c90a2bfc86c7b3d4da6b733bf
unknown <cogjava213@iiht.tech>
2022-05-20 17:24:05 +0530

    updated the port number to run in local

--- estockmarket-query/src/main/java/com/estockmarket/query/application/config/KafkaConsumerConfiguration.java
+++ estockmarket-query/src/main/java/com/estockmarket/query/application/config/KafkaConsumerConfiguration.java
@@ -19,7 +19,7 @@ public class KafkaConsumerConfiguration {
 	public ConsumerFactory<String, String> userConsumerFactory() {
 	    Map<String, Object> config = new HashMap<>();
 
-	    config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:29092");
+	    config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:29094");
 	    config.put(ConsumerConfig.GROUP_ID_CONFIG, "group_id");
 	    config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 	    config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
